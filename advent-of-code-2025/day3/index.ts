42eed692ceb1292dff6082c92a5c1a861acc5e1d
David Behar Lombrozo <davbelom@gmail.com>
2025-12-09 11:55:29 -0600

    Day 3, part 1

--- advent-of-code-2025/day3/index.ts
+++ advent-of-code-2025/day3/index.ts
@@ -6,12 +6,33 @@ const __dirname = dirname(
   fileURLToPath(import.meta.url),
 );
 
-const inputFile = path.resolve(__dirname, 'sample-input.txt')
+const inputFile = path.resolve(__dirname, 'input.txt')
 
 const data = (await fs.readFile(inputFile, 'utf8')).split('\n');
 
-function part1(input: string[]) {
-  console.log({ input })
+
+const getMax =
+  (input: string): number => Math.max(
+    ...input.split('').map(Number)
+  )
+
+function getJoltage(bank: string): number {
+  if (!bank) return 0
+
+  const bankString = bank.toString()
+  const max1 = getMax(bankString.substring(0, bankString.length - 1))
+  const max2 = getMax(bankString.substring(
+    bankString.indexOf(max1 + "") + 1)
+  )
+
+  if (!Number('' + max1 + max2)) console.log({ bank, joltage: Number('' + max1 + max2) })
+  return Number('' + max1 + max2)
+
 }
 
-part1(data)
+
+const part1 = (banks: string[]) => banks
+  .map(bank => getJoltage(bank))
+  .reduce((acc, el) => acc + el, 0)
+
+console.log(part1(data))

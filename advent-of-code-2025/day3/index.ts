f80c6f41b4ea69a8242c66fcd3c965f7635da7e5
David Behar Lombrozo <davbelom@gmail.com>
2025-12-09 16:38:48 -0600

    Day 3, part 2, incorrect

--- advent-of-code-2025/day3/index.ts
+++ advent-of-code-2025/day3/index.ts
@@ -1,3 +1,5 @@
+// Too low 158840668273292
+
 import fs from 'node:fs/promises'; // async
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -6,33 +8,58 @@ const __dirname = dirname(
   fileURLToPath(import.meta.url),
 );
 
-const inputFile = path.resolve(__dirname, 'input.txt')
-
-const data = (await fs.readFile(inputFile, 'utf8')).split('\n');
 
 
-const getMax =
-  (input: string): number => Math.max(
-    ...input.split('').map(Number)
-  )
+
+
+function getCleanJoltage(slices: (number)[]): number[] {
+  let min = Math.min(...slices.filter(el => el !== null))
+
+  while (slices.length > 12) {
+    const nextMin = slices.indexOf(min)
+    slices.splice(nextMin, 1)
+  }
+  return slices
+}
+
+const findStartingIndex = (bankNumbers: number[]): number => {
+  let left = 0, right = bankNumbers.length, currentIndex = 0;
+
+  while ((right - left) > 12 && currentIndex < bankNumbers.length) {
+    if (bankNumbers[currentIndex]! > bankNumbers[left]!) {
+      left = currentIndex;
+    }
+    else right--
+    currentIndex++;
+  }
+
+  return left
+}
+
 
 function getJoltage(bank: string): number {
   if (!bank) return 0
 
-  const bankString = bank.toString()
-  const max1 = getMax(bankString.substring(0, bankString.length - 1))
-  const max2 = getMax(bankString.substring(
-    bankString.indexOf(max1 + "") + 1)
-  )
+  const bankNumbers = bank.split('').map(Number)
+  const left = findStartingIndex(bankNumbers)
+  const numbersFromLeft = bankNumbers.slice(left)
+  const clean = getCleanJoltage(numbersFromLeft)
+    .join('')
 
-  if (!Number('' + max1 + max2)) console.log({ bank, joltage: Number('' + max1 + max2) })
-  return Number('' + max1 + max2)
-
+  return Number(clean)
 }
 
+function part2  (banks: string[]) {
+  return banks
+    .map(getJoltage)
+    .reduce((acc, el) => acc + el, 0)
+}
+const inputFile = path.resolve(__dirname, 'sample-input.txt')
+const data = (await fs.readFile(inputFile, 'utf8')).split('\n');
 
-const part1 = (banks: string[]) => banks
-  .map(bank => getJoltage(bank))
-  .reduce((acc, el) => acc + el, 0)
 
-console.log(part1(data))
+
+console.log(part2(data))
+
+
+// console.log('removeMin(data)', removeMin('811111111111119'))

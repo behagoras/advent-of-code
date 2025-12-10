166c02759146cd20eab34d7fd4e7915a0405bb9d
David Behar Lombrozo <davbelom@gmail.com>
2025-12-10 16:26:01 -0600

    ♻️ Day 4, part 2, create reusable functions

--- advent-of-code-2025/day04/part2.ts
+++ advent-of-code-2025/day04/part2.ts
@@ -3,62 +3,71 @@ import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const setCharAt = (str: string, index: number, char: string) =>
-  index < 0 || index >= str.length
-    ? str
-    : str.substring(0, index) + char + str.substring(index + 1)
+const rollsChar = "@"
 
-function removeCoordinates(coordinatesToRemove: [number, number][], grid: string[]) {
+function removeCoordinates(coordinatesToRemove: [number, number][], grid: string[],) {
+  const setCharAt = (str: string, index: number, char: string) =>
+    index < 0 || index >= str.length
+      ? str
+      : str.substring(0, index) + char + str.substring(index + 1)
+
   for (let [x, y] of coordinatesToRemove) grid[y] = setCharAt(grid[y]!, x, ".")
 
   return grid
 }
 
+function countMatchesAndUpdateCoordinates(
+  grid: string[],
+  [x, y]: [number, number],
+  coordinatesToRemove: [number, number][]
+) {
+  let count = 0, repeated = 0
+  const coordinates = [
+    [x - 1, y - 1],
+    [x - 1, y],
+    [x - 1, y + 1],
+    [x, y - 1],
+    // [x, y],
+    [x, y + 1],
+    [x + 1, y - 1],
+    [x + 1, y],
+    [x + 1, y + 1],
+  ]
+
+  for (let [x, y] of coordinates) {
+    if (repeated >= 4) break
+
+    const currentElement = grid?.[y!]?.[x!] ?? '.'
+    if (currentElement === rollsChar) repeated++
+  }
+
+  if (repeated < 4 && grid?.[y]?.[x]) {
+    coordinatesToRemove.push([x, y])
+    count++
+  }
+
+  return count
+}
+
 function getCounts(grid: string[]): [[number, number][], number] {
-  const rollsChar = "@"
   const width = grid[0]!.length
   let count = 0
   let coordinatesToRemove: [number, number][] = []
-  for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < width; col++) {
-      const current = grid[row]![col]
-      // console.log({current})
-      if (current !== rollsChar) continue
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < width; x++) {
+      if (grid[y]![x] !== rollsChar) continue
 
-      let repeated = 0
-      const coordinates = [
-        [col - 1, row - 1],
-        [col - 1, row],
-        [col - 1, row + 1],
-        [col, row - 1],
-        // [x, y],
-        [col, row + 1],
-        [col + 1, row - 1],
-        [col + 1, row],
-        [col + 1, row + 1],
-      ]
-
-      for (let [x, y] of coordinates) {
-        if (repeated >= 4) break
-
-        const currentElement = grid?.[y!]?.[x!] ?? '.'
-        if (currentElement === rollsChar) repeated++
-      }
-
-      if (repeated < 4 && grid?.[row]?.[col]) {
-        coordinatesToRemove.push([col, row])
-        count++
-      }
+      count += countMatchesAndUpdateCoordinates(grid, [x, y], coordinatesToRemove)
     }
   }
   return [coordinatesToRemove, count]
 }
 
 // Day 04 part 2, example output ->
-const part2 = (grid: string[]): number => {
-  let count = 0,
-    first = true,
-    coordinatesToRemove: [number, number][] = [];
+function part2(grid: string[]): number {
+  let count = 0
+  let first = true
+  let coordinatesToRemove = [] as [number, number][]
 
   while (coordinatesToRemove.length || first) {
     first = false
@@ -74,5 +83,5 @@ const part2 = (grid: string[]): number => {
 
 const inputFile = path.resolve(__dirname, 'input.txt');
 const data = (await fs.readFile(inputFile, 'utf8')).split('\n').filter(Boolean);
-// console.log(JSON.stringify(data, null, 2))
+
 console.log(part2(data));
